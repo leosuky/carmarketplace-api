d922d5f39f5567b524ceb9057eb14faf2a42d5a2
Sunkanmi Otitolaye <60078601+leosuky@users.noreply.github.com>
2020-05-09 13:59:51 +0100

    last commit

--- MLApi/settings.py
+++ MLApi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!nu3f=^+4@-u^pd^41qbf8=w8d=&%ea1@usphi@2%w*9f4q(l#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['carmarketplace-api.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['carmarketplace-api.herokuapp.com', '127.0.0.1']
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Application definition
@@ -121,7 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'CarsApi/static')]

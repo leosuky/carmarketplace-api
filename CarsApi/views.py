230499c0dcdb748fc1660e11d44fd600e90855e2
Sunkanmi Otitolaye <60078601+leosuky@users.noreply.github.com>
2021-09-26 23:05:19 +0100

    Update views.py
    updated views

--- CarsApi/views.py
+++ CarsApi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from sklearn.externals import joblib
+import joblib
 from django.contrib import messages
 import numpy as np
 import pandas as pd
